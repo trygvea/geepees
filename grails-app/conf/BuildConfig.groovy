9e6e753f79e26bf52cbed25b7e1a65ba7aa2738e
trygvea <trygve.amundsen@gmail.com>
2011-02-03 00:49:34 +0100

    Prepared for sending REST messages

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,8 +16,8 @@ grails.project.dependency.resolution = {
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
+        mavenLocal()
+        mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
+        compile 'commons-httpclient:commons-httpclient:3.1'
         // runtime 'mysql:mysql-connector-java:5.1.5'
     }
 }
--- grails-app/jobs/LookForNewPositionsJob.groovy
+++ grails-app/jobs/LookForNewPositionsJob.groovy
@@ -16,6 +16,7 @@ class LookForNewPositionsJob {
             movingDevices.each {
                 moveRandomly(it)
             }
+            sendMessage(movingDevices)
         }
     }
     
@@ -30,5 +31,25 @@ class LookForNewPositionsJob {
             whenReported: new Date(), 
             what: "Device "+device.idExternal +" moved to (lat,long)="+device.currentLatitude+","+device.currentLongitude, 
         ).save(flush:true, failOnError:true)
+    }
+    
+    private def sendMessage(devices) {
+/*        HttpClient httpClient = new HttpClient()
+        GetMethod method = new GetMethod(realUrl.toString());
+        try {
+            int statusCode = httpClient.executeMethod(method);
+            if (statusCode != HttpStatus.SC_OK) {
+                throw new RuntimeException("HttpClient returned statusCode="+statusCode)
+            }
+            byte[] responseBody = method.getResponseBody();
+            response.contentType = "image/png"
+            response.outputStream << responseBody
+        } catch (Exception ex) {
+            println "####################"
+            println "Response:" + method.responseBody
+            ex.printStackTrace()
+            throw ex
+        } 
+*/		
     }
 }
